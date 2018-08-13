434c2a5a354a44ef3e511dfa86f97ac1667f60a2
sakurahe <l>
2018-08-13 18:08:52 +0800

    sakurahe

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    紧急开发中...
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
